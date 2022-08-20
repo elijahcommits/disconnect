42e3123e68c38ce75f9e41c115dd2a5ca0d4de37
Elijah <seldomrealized@gmail.com>
2022-08-19 23:09:55 -0700

    switch to hsl color generator; improve readability

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ const disconnectFontFamily = disconnect.style.fontFamily;
 const fontList = ['Noer Mala Script', 'Noer Mala Script slant', 'Perfume', 'Rock n Doll Script', 'The Paragraph Script', 'CopperplateCC', 'BodoniModa06pt-Book', 'EngravingCC', 'RailroadGothicCC', 'EngravingUnshadedCC', 'Jost-Book', 'notearsbold', 'gnomon-it-foreground', 'DraftingMono-Regular', 'Besley-Fatface', 'TiffanyGothicCC'];
 
 
-const randomColor = () => {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  disconnect.style.color = "#" + randomColor;
-}
-
+const getRandomColor = () => {
+  const h = Math.round(Math.random() * 360),  // max value of h is 360
+        s = Math.round(Math.random() * 100) + '%',  // max value of s is 100%
+        l = (Math.floor(Math.random() * 75) + 15) + '%';  // max value of l is 100%
+  return `hsl(${h},${s},${l})`;
+};
 
 let lastNumber = 0;
-const randomNumber = (array) => {
-  let getRandomNumber = Math.floor(Math.random() * array.length);  
-  if (getRandomNumber != lastNumber) {
-      let random = array[getRandomNumber];
-      disconnect.style.fontFamily = random;
-      lastNumber = getRandomNumber;
+const getRandomNumber = (array) => {
+  let randomNumber = Math.floor(Math.random() * array.length);  
+  if (randomNumber != lastNumber) {
+      let random = array[randomNumber];
+      lastNumber = randomNumber;
+      return random;
       // console.log(random);
   } else {
-      randomNumber(array);
-      // console.log(randomNumber(array));
+      getRandomNumber(array);
+      // console.log(getRandomNumber(array));
   }
 };
 
-function newFont(array) {
-  randomNumber(fontList);
-  randomColor();
+function changeText(array) {
+  disconnect.style.fontFamily = getRandomNumber(fontList);
+  disconnect.style.color = getRandomColor();
 }
 
-document.body.addEventListener('click', newFont);
+document.body.addEventListener('click', changeText);
